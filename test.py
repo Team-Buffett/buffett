30d7dd453581244e5fc4a4d09be4d9f44bc8d047
dgh06175 <dgh06175@gmail.com>
2025-04-05 13:55:02 +0900

    test: api key 불러오기 테스트

--- test.py
+++ test.py
@@ -1,5 +1,12 @@
-print("Hello World!")
+from dotenv import load_dotenv
+import os
 
+load_dotenv("buffett-config/.env")
 
-print("tttt")
+openai_api_key = os.getenv("OPANAI_API_KEY")
+api_key = os.getenv("BINANCE_API_KEY")
+secret_key = os.getenv("BINANCE_SECRET_KEY")
 
+print(openai_api_key)
+print(api_key)
+print(secret_key)
